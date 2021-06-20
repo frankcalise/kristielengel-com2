e4be4c32d784033b30bbf3940a2575054e3006a0
Frank Calise <fcalise@gmail.com>
2021-06-20 19:53:49 -0400

    feature(nav): mobile nav

---
+++ src/components/Nav/Logo.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import whiteLogo from "../../images/KL_Logo_WHITE.png";
+
+const Logo = () => (
+  <div className="logo-wrap">
+    <img src={whiteLogo} alt="KL logo" />
+  </div>
+);
+
+export default Logo;
